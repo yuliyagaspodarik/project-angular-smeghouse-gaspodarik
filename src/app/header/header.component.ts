7277d3dfb4b659966252ffb774c41b3c86880355
Yuliya Gaspodarik <gaspod.yu@gmail.com>
2020-10-11 16:25:54 +0300

    Add search

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {debounceTime} from "rxjs/operators";
+import {ProductsService} from "../products.service";
+import {fromEvent, Subscription} from "rxjs";
+import {Products} from "../products.interface";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy {
+  subscription: Subscription;
+  searchedProducts: Products[];
+  @ViewChild('search') search: ElementRef;
 
-  constructor() { }
+  constructor(private productsService: ProductsService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.subscription = fromEvent(this.search.nativeElement, 'keyup').pipe(debounceTime(3000)).subscribe(() => {
+      this.searchedProducts = this.productsService.getProducts().filter(product => product.name.toLowerCase().indexOf(this.search.nativeElement.value.toLowerCase()) !== -1);
+      console.log(this.searchedProducts);
+    })
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
