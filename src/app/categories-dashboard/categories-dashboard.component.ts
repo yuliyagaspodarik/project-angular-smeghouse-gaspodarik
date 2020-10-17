dbabc880dc76390c2897a9cde11da24271c7fb1c
Yuliya Gaspodarik <gaspod.yu@gmail.com>
2020-10-17 04:35:00 +0300

    Correct transition to categories

--- src/app/categories-dashboard/categories-dashboard.component.ts
+++ src/app/categories-dashboard/categories-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductsService} from "../products.service";
 
 @Component({
@@ -11,13 +11,17 @@ export class CategoriesDashboardComponent implements OnInit {
   categoriesLeft: string[];
   categoriesRight: string[];
 
-  constructor(private productsService: ProductsService) {}
+  constructor(private productsService: ProductsService) {
+  }
 
   ngOnInit(): void {
     this.categories = this.productsService.getCategories().sort();
     const dimensionCategories = this.categories.length;
-    this.categoriesLeft = this.categories.slice(0, Math.ceil(dimensionCategories/2));
-    this.categoriesRight = this.categories.slice(Math.ceil(dimensionCategories/2), dimensionCategories);
+    this.categoriesLeft = this.categories.slice(0, Math.ceil(dimensionCategories / 2));
+    this.categoriesRight = this.categories.slice(Math.ceil(dimensionCategories / 2), dimensionCategories);
   }
 
+  checkCategory(category) {
+    this.productsService.searchedProducts = this.productsService.getProducts().filter(product => product.category === category);
+  }
 }
